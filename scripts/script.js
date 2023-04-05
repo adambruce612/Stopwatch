13c42dc4e9e33e552d6b4b96535f22c70f037ed2
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-04-05 09:43:25 -0500

    Add files via upload
    HTML and beginning of styling completed. Buttons selected but need to do rest of the JS still.

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const resetBtn = document.getElementById('reset');
+
+console.log(startBtn);
+console.log(stopBtn);
+console.log(resetBtn);
