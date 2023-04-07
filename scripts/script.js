56fc3bc3e43803dd85523f6f5774786d236efb47
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-04-07 09:39:52 -0500

    Add files via upload
    Updated CSS/added Event Listeners in JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,27 @@ const startBtn = document.getElementById('start');
 const stopBtn = document.getElementById('stop');
 const resetBtn = document.getElementById('reset');
 
-console.log(startBtn);
-console.log(stopBtn);
-console.log(resetBtn);
+let hour = 00;
+let minute = 00;
+let second = 00;
+let count = 00;
+
+startBtn.addEventListener('click', function() {
+    timer = true;
+    stopWatch();
+});
+
+stopBtn.addEventListener('click', function() {
+    timer = false;
+});
+
+resetBtn.addEventListener('click', function() {
+    timer = false;
+    hour = 0;
+    minute = 0;
+    second = 0;
+    count = 0;
+    document.getElementById('hour').innerHTML = "00";
+    document.getElementById('minute').innerHTML = "00";
+    document.getElementById('second').innerHTML = "00";
+});
